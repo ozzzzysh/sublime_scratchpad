8663d3fc0e2ca9b07ea70abd9caf7628afbe32a6
Sidney Hays <sidneyh@solution-source.net>
2021-08-25 12:52:11 -0400

    Customized functionality.

--- Scratchpad.py
+++ Scratchpad.py
@@ -1,7 +1,8 @@
 from sublime_plugin import WindowCommand, TextCommand
-from sublime import packages_path, run_command, ENCODED_POSITION
+from sublime import packages_path, run_command, ENCODED_POSITION, set_timeout
 from time import strftime
 from os.path import isfile
+import os
 
 headerText = """
     _____                _       _                     _
@@ -10,8 +11,8 @@ headerText = """
     `--. \/ __| '__/ _` | __/ __| '_ \| '_ \ / _` |/ _` |
    /\__/ / (__| | | (_| | || (__| | | | |_) | (_| | (_| |
    \____/ \___|_|  \__,_|\__\___|_| |_| .__/ \__,_|\__,_|
-                                      | |
-                                      |_|
+                                      | |                
+                                      |_|                
 
 """
 
@@ -20,6 +21,14 @@ class OpenScratchpadCommand(WindowCommand):
     scratchpadFile = packages_path()[:-8]+'scratchpad.txt'
     checkAndFillEmpty(scratchpadFile)
     self.window.open_file(scratchpadFile)
+    # self.window.open_file(scratchpadFile+':'+str(sum(1 for line in scratchpadFile)), ENCODED_POSITION)
+    # def preview():
+    #   self.window.run_command("open_markdown_preview")
+    # set_timeout(preview, 100)
+    print(os.linesep)
+    def foldAll():
+      self.window.run_command("fold_all_sections", {"target_level": 1})
+    set_timeout(foldAll, 100)
 
 class ScratchpadCommand(WindowCommand):
   def run(self):
@@ -28,6 +37,13 @@ class ScratchpadCommand(WindowCommand):
     checkAndFillEmpty(scratchpadFile)
     count = putTimeStamp(scratchpadFile)
     self.window.open_file(scratchpadFile+':'+str(count+1), ENCODED_POSITION)
+    # def preview():
+    #   self.window.run_command("open_markdown_preview")
+    # set_timeout(preview, 100)
+    def foldAll():
+      self.window.run_command("fold_all_sections", {"target_level": 1})
+      self.window.run_command("go_to_line", {"line":0})
+    set_timeout(foldAll, 100)
 
 def checkAndFillEmpty(scratchpadFile):
   global headerText
@@ -36,7 +52,7 @@ def checkAndFillEmpty(scratchpadFile):
       scratchFile.write(headerText)
 
 def putTimeStamp(scratchpadFile):
-  timeStamp = "\n\n" + strftime("%c") + " : " + "\n" +"========================" + "\n"
+  timeStamp = "\n\n" + strftime("%x") + "\n" +"========================" + "\n"
   with open(scratchpadFile, "a") as scratchFile:
       scratchFile.write(timeStamp)
   with open(scratchpadFile) as scratchFile:
